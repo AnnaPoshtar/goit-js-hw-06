480f0cc4803d05d82df27d9c3dc50c113c3c60fa
Anna Poshtar <annaposhtar11@gmail.com>
2024-05-31 20:03:10 +0200

    start

--- js/task-3.js
+++ js/task-3.js
@@ -1,27 +1,16 @@
-class StringBuilder {
-  #value;
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
-  getValue() {
-    return this.#value;
-  }
-  padEnd(str) {
-    this.#value += str;
-  }
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-  padBoth(str) {
-    this.#value = str + this.#value + str;
-  }
-}
+const nameInput =
+    document.querySelector('#name-input');
 
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+const nameOutput =
+    document.querySelector('#name-output');
+
+nameInput.addEventListener('input', () => {
+    const trimmedValue =
+        nameInput.value.trim();
+    
+    if (trimmedValue) {
+        nameOutput.textContent = trimmedValue;
+    } else {
+        nameOutput.textContent = 'Anonymous';
+    }
+});
